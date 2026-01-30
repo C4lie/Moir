3153722a306b4875f16f35fc5190de30893532a3
C4lie <chandril.halder0@gmail.com>
2026-01-31 03:51:39 +0530

    feat: Configure Django to serve React frontend

--- backend/settings.py
+++ backend/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/dist/assets',
+]
 
 # Media files
 MEDIA_URL = '/media/'
