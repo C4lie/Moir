b2aa2cc1a08c46fb0b59e2b4e6b4cc4612a48958
C4lie <chandril.halder0@gmail.com>
2026-01-31 11:01:05 +0530

    fix: Use CompressedStaticFilesStorage for Vercel

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/dist',
